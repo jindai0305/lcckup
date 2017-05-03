c2c22f7d9e0f80276b7da5b0c48a14f6c1ea8a9d
jindai0305 <cc520820lk@163.com>
2017-05-03 21:27:21 +0800

    del mysql

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'app_blog',
+    'database'        => '',
     // 用户名
-    'username'        => 'lcck',
+    'username'        => '',
     // 密码
-    'password'        => 'lcck0305blog',
+    'password'        => '',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
